fc8192a1e62cb21187f58a93213b0286ad50a545
sahlannaufal <sahlannaufal.sn@gmail.com>
2025-02-25 15:03:18 +0700

    fref

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Semua request ke /api/*
+        destination: "http://103.149.177.11/:path*", // Diteruskan ke backend HTTP
+      },
+    ];
+  },
+};
