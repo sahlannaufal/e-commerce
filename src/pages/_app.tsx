82bb7616c015e44873d940bcbf49f1568bee39e0
sahlannaufal <sahlannaufal.sn@gmail.com>
2025-02-25 16:33:56 +0700

    google anal

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,44 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import ContextProvider from "@contexts/ContextProvider";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import Script from "next/script";
 
+const GA_TRACKING_ID = "G-L10K70P19F";
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      (window as any).gtag("config", GA_TRACKING_ID, {
+        page_path: url,
+      });
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ContextProvider>
+      {/* Google Analytics Script */}
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
+
       <Component {...pageProps} />
     </ContextProvider>
   );
